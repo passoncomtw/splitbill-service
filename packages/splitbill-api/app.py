eaa473323ac87889c2cb9955bd756e79ecc1eb6c
RobYang203 <101m06014@stud.sju.edu.tw>
2024-08-18 04:04:24 +0800

    feat(api): add flask_restx

--- packages/splitbill-api/app.py
+++ packages/splitbill-api/app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from flask_restx import Api, Resource, Namespace
+
 app = Flask(__name__)
+api = Api(app, version='0.0', title='Split bill API',
+          description='拆帳系統 API',  doc='/doc')
+
+hello = Namespace("Hello")
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@hello.route("")
+class HelloWorld(Resource):
+    def get(self):
+        return {'message': 'Hello, World!'}
 
+
+api.add_namespace(hello, '/hello')
 
 if __name__ == '__main__':
     app.run()
