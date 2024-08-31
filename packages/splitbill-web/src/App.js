1a74fcbc3b6008f4d40bcb82e997c7650db371b0
TomasLin <horsekit1982@gmail.com>
2024-08-31 13:07:15 +0800

    feat(#6 修正 dev server refresh 無法顯示的問題):

--- packages/splitbill-web/src/App.js
+++ packages/splitbill-web/src/App.js
@@ -13,29 +13,8 @@ import { fakeAuthProvider } from "./constants/auth";
 export default function App() {
   return (
     <AuthProvider>
-      <h1>Auth Example</h1>
-
-      <p>
-        This example demonstrates a simple login flow with three pages: a public
-        page, a protected page, and a login page. In order to see the protected
-        page, you must first login. Pretty standard stuff.
-      </p>
-
-      <p>
-        First, visit the public page. Then, visit the protected page. You're not
-        yet logged in, so you are redirected to the login page. After you login,
-        you are redirected back to the protected page.
-      </p>
-
-      <p>
-        Notice the URL change each time. If you click the back button at this
-        point, would you expect to go back to the login page? No! You're already
-        logged in. Try it out, and you'll see you go back to the page you
-        visited just *before* logging in, the public page.
-      </p>
-
       <Routes>
-        <Route element={<Layout />}>
+        <Route exact element={<Layout />}>
           <Route path="/" element={<PublicPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route
