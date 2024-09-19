64f115cfdad0ad95c167bd803a7c360d862970f0
PassonoTW <82664528+passontw@users.noreply.github.com>
2024-09-20 01:39:34 +0800

    feat: #32 登入頁面

--- packages/splitbill-web/src/screens/LoginScreen/view.js
+++ packages/splitbill-web/src/screens/LoginScreen/view.js
@@ -43,7 +43,7 @@ const LoginScreen = () => {
 
   return (
     <Flex bg="gray.100" align="center" justify="center" h="100vh">
-      <Box bg="white" p={6} rounded="md" w={64}>
+      <Box bg="white" p={6} rounded="md">
         <LineLogin
         clientID='2006210817'
         clientSecret='0370031cdd35afb16fba1fa540990cbd'
@@ -53,56 +53,6 @@ const LoginScreen = () => {
         setPayload={setPayload}
         setIdToken={setIdToken}
         />
-      <Formik
-            initialValues={{
-              email: "",
-              password: "",
-              rememberMe: false
-            }}
-            onSubmit={(values) => {
-              alert(JSON.stringify(values, null, 2));
-            }}
-          >
-            {({ handleSubmit, errors, touched }) => (
-              <form onSubmit={handleSubmit}>
-                <VStack spacing={4} align="flex-start">
-                  <FormControl>
-                    <FormLabel htmlFor="phone">Phone Number</FormLabel>
-                    <Field
-                      as={Input}
-                      id="phone"
-                      name="phone"
-                      type="phone"
-                      variant="filled"
-                    />
-                  </FormControl>
-                  <FormControl isInvalid={!!errors.password && touched.password}>
-                    <FormLabel htmlFor="password">Password</FormLabel>
-                    <Field
-                      as={Input}
-                      id="password"
-                      name="password"
-                      type="password"
-                      variant="filled"
-                      validate={(value) => {
-                        let error;
-  
-                        if (value.length < 6) {
-                          error = "Password must contain at least 6 characters";
-                        }
-  
-                        return error;
-                      }}
-                    />
-                    <FormErrorMessage>{errors.password}</FormErrorMessage>
-                  </FormControl>
-                  <Button type="submit" colorScheme="purple" width="full">
-                    Login
-                  </Button>
-                </VStack>
-              </form>
-            )}
-          </Formik>
       </Box>
     </Flex>
   );
--- packages/splitbill-web/webpack.config.js
+++ packages/splitbill-web/webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
       main: './src/main.js',
       index: './src/index.html',
       baseHref: '/',
-      assets: ['./src/favicon.ico', './src/assets'],
+      assets: ['./src/favicon.ico'],
       styles: ['./src/styles.css'],
       outputHashing: process.env['NODE_ENV'] === 'production' ? 'all' : 'none',
       optimization: process.env['NODE_ENV'] === 'production',
